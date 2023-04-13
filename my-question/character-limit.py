b696cd8994b78e649bbcb0cc8e993bce4b417862
Sergiorq1 <sergiorq12016@gmail.com>
2023-04-13 13:58:13 -0600

    added another test input for my-question

--- my-question/character-limit.py
+++ my-question/character-limit.py
@@ -1,10 +1,20 @@
 # Question
 # Given a character limit and a sentence, return a new sentence with “ % ”  separating each character limit 
 
-# Input
-# test input: 280, 'Federal Reserve policymakers scaled back their expectations for rate hikes this year after a series of bank collapses roiled markets last month, and stressed they would remain vigilant for the potential of a credit crunch to further slow the economy, a record of the meeting showed. “Many participants noted that the likely effects of recent banking-sector developments on economic activity and inflation had led them to lower their assessments of the federal funds rate target range that would be sufficiently restrictive,” according to minutes of the March 21-22 Federal Open Market Committee gathering, released in Washington Wednesday.'
-# Output
+# Example 1
+
+# input: 280, 'Federal Reserve policymakers scaled back their expectations for rate hikes this year after a series of bank collapses roiled markets last month, and stressed they would remain vigilant for the potential of a credit crunch to further slow the economy, a record of the meeting showed. “Many participants noted that the likely effects of recent banking-sector developments on economic activity and inflation had led them to lower their assessments of the federal funds rate target range that would be sufficiently restrictive,” according to minutes of the March 21-22 Federal Open Market Committee gathering, released in Washington Wednesday.'
+
 # output: Federal Reserve policymakers scaled back their expectations for rate hikes this year after a series of bank collapses roiled markets last month, and stressed they would remain vigilant for the potential of a credit crunch to further slow the economy, a record of the meeting showed. “Many participants noted that the likely effects  %  of recent banking-sector developments on economic activity and inflation had led them to lower their assessments of the federal funds rate target range that would be sufficiently restrictive,” according to minutes of the March 21-22 Federal Open Market Committee gathering, released in Washington Wednesday.
+
+
+#Example 2
+
+# input: 50, 'Federal Reserve policymakers scaled back their expectations for rate hikes this year after a series of bank collapses'
+
+# output 
+#output: Federal Reserve policymakers scaled back their  %  expectations for rate hikes this year after a series of bank  %  collapses
+
 
 class Solution:
     def character_limit(self, limit:int, paragraph:str):
